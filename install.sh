8807717872c707831b3ed52ba883b8289b44b46c
renemayer-hb <renemayer@renemayer.de>
2020-03-21 21:14:53 +0100

    initial

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+rpi-rw
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+git clone https://github.com/n0mjs710/dmr_utils
+git clone https://github.com/n0mjs710/HBLink
+cd dmr_utils
+sudo ./install.sh
+cd ..
+cd HBLink/
+sh ./install.sh
+cd ..
+sudo cp ipv4.fw /root/ipv4.fw
+sudo pistar-firewall
+sudo cp hblink_bridgeall.service /etc/systemd/system/hblink_bridgeall.service
+sudo systemctl enable hblink_bridgeall.service
+sudo systemctl start hblink_bridgeall.service
