b90f7bc0ccced1e56fcdee89a5a780b0c2638897
renemayer-hb <renemayer@renemayer.de>
2020-03-21 22:57:34 +0100

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -8,12 +8,12 @@ git clone https://github.com/n0mjs710/hblink3.git
 cd dmr_utils3
 sudo ./install.sh
 cd ..
-cp hblink_bridgeall.cfg ./hblink_bridgeall.cfg
+cp hblink3_bridgeall.cfg ./hblink3_bridgeall.cfg
 cd hblink3/
 sh ./install.sh
 cd ..
 sudo cp ipv4.fw /root/ipv4.fw
 sudo pistar-firewall
-sudo cp hblink_bridgeall.service /etc/systemd/system/hblink_bridgeall.service
-sudo systemctl enable hblink_bridgeall.service
-sudo systemctl start hblink_bridgeall.service
+sudo cp hblink3_bridgeall.service /etc/systemd/system/hblink3_bridgeall.service
+sudo systemctl enable hblink3_bridgeall.service
+sudo systemctl start hblink3_bridgeall.service
