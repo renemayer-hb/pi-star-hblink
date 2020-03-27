fac149b3b09ce0d003625c3563cdf70da7955da8
Rene Mayer <renemayer@renemayer.de>
2020-03-27 20:56:00 +0100

    change to bridge

--- install3.sh
+++ install3.sh
@@ -5,11 +5,15 @@ sudo apt upgrade
 sudo apt install git
 git clone https://github.com/n0mjs710/dmr_utils3.git
 git clone https://github.com/n0mjs710/hblink3.git
+git clone https://github.com/kc1awv/hbmonitor3.git
 sudo sh ./dmr_utils3/install.sh
 sudo sh ./hblink3/install.sh
-cp rules.py ./hblink3/rules.py
+sudo sh ./hbmonitor3/install.sh
 sudo cp ipv4.fw /root/ipv4.fw
 sudo pistar-firewall
-sudo cp hblink3_bridgeall.service /etc/systemd/system/hblink3_bridge.service
+sudo cp hblink3_bridge.service /etc/systemd/system/hblink3_bridge.service
 sudo systemctl enable hblink3_bridge.service
 sudo systemctl start hblink3_bridge.service
+sudo cp hbmonitor3.service /etc/systemd/system/hbmonitor3.service
+sudo systemctl enable hbmonitor3.service
+sudo systemctl start hbmonitor3.service
