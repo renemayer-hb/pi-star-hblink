2d0d84581077466658d39df26a3130588d526f0f
Rene Mayer <renemayer@renemayer.de>
2020-03-27 13:30:28 +0000

    irgendwas

--- install3.sh
+++ install3.sh
@@ -5,13 +5,8 @@ sudo apt upgrade
 sudo apt install git
 git clone https://github.com/n0mjs710/dmr_utils3.git
 git clone https://github.com/n0mjs710/hblink3.git
-cd dmr_utils3
-sudo sh ./install.sh
-cd ..
-cp hblink3_bridgeall.cfg ./hblink3/hblink3_bridgeall.cfg
-cd hblink3
-sudo sh ./install.sh
-cd ..
+sudo sh ./dmr_utils3/install.sh
+sudo sh ./hblink3/install.sh
 sudo cp ipv4.fw /root/ipv4.fw
 sudo pistar-firewall
 sudo cp hblink3_bridgeall.service /etc/systemd/system/hblink3_bridgeall.service
