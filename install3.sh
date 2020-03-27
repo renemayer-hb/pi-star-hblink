e05b25de19e806bc7004fc98254461439c54f796
Rene Mayer <renemayer@renemayer.de>
2020-03-27 20:43:39 +0100

    change to bridge

--- install3.sh
+++ install3.sh
@@ -7,8 +7,9 @@ git clone https://github.com/n0mjs710/dmr_utils3.git
 git clone https://github.com/n0mjs710/hblink3.git
 sudo sh ./dmr_utils3/install.sh
 sudo sh ./hblink3/install.sh
+cp rules.py ./hblink3/rules.py
 sudo cp ipv4.fw /root/ipv4.fw
 sudo pistar-firewall
-sudo cp hblink3_bridgeall.service /etc/systemd/system/hblink3_bridgeall.service
-sudo systemctl enable hblink3_bridgeall.service
-sudo systemctl start hblink3_bridgeall.service
+sudo cp hblink3_bridgeall.service /etc/systemd/system/hblink3_bridge.service
+sudo systemctl enable hblink3_bridge.service
+sudo systemctl start hblink3_bridge.service
